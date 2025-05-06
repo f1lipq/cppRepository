f4b164d66f10533b756de93e0be57d505acc1526
Filip Jamróz <fjamroz9@gmail.com>
2025-05-06 09:04:15 +0000

    First code in C++

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+void my_print(string text){
+    cout << text << endl;
+}
+
+
+int main(){
+    string text = "Ala";
+
+    my_print("Ala ma kota");
+    cout << "Podaj tekst" << endl;
+    getline(cin, text);
+    my_print("Nowy tekst = " + text);
+    return 0;
+}
