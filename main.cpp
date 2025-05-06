4aaeefa57d6efede8d30929a854fb7d18c399fd8
Filip Jamróz <fjamroz9@gmail.com>
2025-05-06 09:15:35 +0000

    added functions

--- main.cpp
+++ main.cpp
@@ -6,13 +6,16 @@ void my_print(string text){
     cout << text << endl;
 }
 
+void menu(string &text){
+    my_print("Podaj tekst");
+    getline(cin, text);
+}
 
 int main(){
     string text = "Ala";
 
     my_print("Ala ma kota");
-    cout << "Podaj tekst" << endl;
-    getline(cin, text);
+    menu(text);
     my_print("Nowy tekst = " + text);
     return 0;
 }
